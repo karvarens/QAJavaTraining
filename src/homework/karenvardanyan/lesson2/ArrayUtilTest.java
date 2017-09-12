cdc10597fce043252c3a200cd8b71b818b532e22
Karen Vardanyan <karenvardanyan@workfront.com>
2017-09-12 17:42:19 +0400

    Added sample for homework.

---
+++ src/homework/karenvardanyan/lesson2/ArrayUtilTest.java
@@ -0,0 +1,12 @@
+package homework.karenvardanyan.lesson2;
+
+public class ArrayUtilTest {
+
+
+    public static void main(String[] args) {
+        // todo : Organize all test methods calls here
+    }
+
+
+    // todo add test methods
+}
