4831e0d07e51308a80df0eca10ca90ba0a098d97
lilitshark <lilit.sharkhatunyan@yahoo.com>
2017-09-15 15:21:32 +0400

    Lesson 2 homework.

---
+++ src/homework/lesson2/lilitsharkhatunyan/util/ArrayUtil.java
@@ -0,0 +1,165 @@
+package homework.lesson2.lilitsharkhatunyan.util;
+import java.util.Arrays;
+
+
+/**
+ * This class contains utility methods for array
+ * manipulation.
+ */
+public class ArrayUtil {
+
+    /**
+     * Sorts the util elements by odd and even number
+     * such that if first number is odd then second must be even number
+     * this means that it will be found the next even number and swap with the second element
+     * if the second element is odd. And so on for the next elements of the util
+     */
+    public static void sortByOddAndEven(int [] array) {
+
+        // int month_days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        // int [] arraySample = {2, 1, 3, 5, 4, 9, 8, 11};
+
+        int length = array.length - 2;
+
+        for (int i = 0; i < length; i++) {
+            int j = i + 1;
+
+                while ((array[i] % 2) == (array[j] % 2)) {
+                    j++;
+                    if (j >= length) {
+                        print(array);
+                        return;
+                    }
+                }
+
+                if (j > i+1) {
+                    swap(array, i +1 , j);
+                }
+        }
+
+    }
+
+    public static int getMaximum(int[] array) {
+
+        int [] arrayA = array;
+
+        Arrays.sort(arrayA);
+        int length = arrayA.length;
+
+        System.out.println("Maximum number is: " + arrayA[length-1]);
+
+        return arrayA[length-1];
+    }
+
+
+    public static int getMinimum(int[] array) {
+        int [] arrayA = array;
+
+        Arrays.sort(arrayA);
+        System.out.println("Minimum number is: " + arrayA[0]);
+
+        return arrayA[0];
+    }
+
+    /**
+     * Calculates the sum of specified <code>array</code>  elements
+     *
+     * @param array specified int array
+     * @return the sum of the specified util elements
+     */
+    public static int getSum(int[] array) {
+
+        int [] arrayA = array;
+        int length = arrayA.length;
+        int sum = 0;
+
+        for(int i = 0; i<length; i++){
+            sum += arrayA [i];
+        }
+
+        System.out.println("Sum of array elements is: " + sum);
+
+        return sum;
+    }
+
+
+    /**
+     * Creates the reversed to specified array new array and returns it.
+     *
+     * @param array  an array corresponding to which must be created reversed array
+     * @return new created reversed array to the specified parameter "array"
+     */
+    public static int[] reverse(int[] array) {
+
+        int length = array.length;
+        int [] result = new int[length];
+
+
+        for (int j = length-1; j >= 0 ; j--) {
+
+            result [j] = array [length - j -1];
+
+        }
+
+        System.out.println("reverse");
+        print(result);
+
+        return result;   // this must be replaced by correct code
+    }
+
+    /**
+     * Reverses the elements order of the specified array
+     * @param array  an array: Elements order of wich must be reversed
+     */
+    public static void reverseInSameArray(int[] array) {
+
+        int length = array.length;
+
+        for (int i = 0; i < length /2 ; i++) {
+            swap(array, i, length / 2);
+
+        }
+
+        System.out.println("reverseInSameArray");
+        print(array);
+    }
+
+    /**
+    * Swaps the elements at the specified positions in the specified array.
+    * (If the specified positions are equal, invoking this method leaves
+    * the array unchanged.)
+    *
+    * @param array The array in which to swap elements.
+    * @param i the index of one element to be swapped.
+    * @param j the index of the other element to be swapped.
+    */
+    private static void swap(int[] array, int i, int j) {
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+
+    public static void print(int[] a) {
+        print(a, " ");
+    }
+
+    /**
+     * Prints all elements in an array by using specified delimiter between elements.
+     *
+     * @param a the util to print
+     */
+
+    public static void print(int[] a, String delimiter) {
+
+        for (int i = 0; i < a.length; i++){
+            System.out.print(a[i] + delimiter);
+        }
+        System.out.println();
+    }
+
+
+}
+      
---
+++ src/homework/lesson2/lilitsharkhatunyan/util/MathUtil.java
@@ -0,0 +1,70 @@
+package homework.lesson2.lilitsharkhatunyan.util;
+
+public class MathUtil {
+    public static void main(String[] args) {
+
+        int [] arraySample = {2, 1, 3, 5, 4, 9, 8, 11};
+        ArrayUtil.sortByOddAndEven(arraySample);
+        ArrayUtil.getMaximum(arraySample);
+        ArrayUtil.getMinimum(arraySample);
+        ArrayUtil.getSum(arraySample);
+        ArrayUtil.reverse(arraySample);
+        ArrayUtil.reverseInSameArray(arraySample);
+    }
+
+    /**
+     * Returns the value of the first argument raised to the power of the
+     * second argument, such that a > 0 and n > 0.
+     *
+     * @param a the base
+     * @param n the exponent
+     * @return the value <code>a<sup>b</sup></code>.
+     */
+    public static int pow(int a, int n) {
+        
+        return 0;
+    }
+
+    /**
+     * Calculates and returns Greatest common divisor
+     *
+     * @param x
+     * @param y
+     * @return gcd of specified x and y numbers
+     */
+    public static int gcd(int x, int y) {
+        
+        return 0;
+    }
+
+    /**
+     * Calculates and returns the factorial of specified n number
+     *
+     * @param n the number factorial of which must be calculated
+     * @return calculated number
+     */
+    public static int factorial(int n) {
+        return 0;
+    }
+
+    /**
+     * Calculates and returns the absolute value
+     * of specified n number
+     *
+     * @param n specified number to find the absolute number
+     * @return absolute number of specified n
+     */
+    public static int abs(int n) {
+        return 0;
+    }
+
+    public static void printIntAsBinary(int n) {
+        //todo complete
+    }
+
+    public static int reverse(int number) {
+        
+        return 0;
+    }
+
+}
