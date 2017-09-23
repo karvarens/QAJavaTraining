ac6d8115c01efffeb210bdece01846e194000cf3
lilitshark <lilit.sharkhatunyan@yahoo.com>
2017-09-23 15:28:32 +0400

    Additional homework 3.

---
+++ src/homework/lesson3/lilitsharkhatunyan/loop/ArrayUtil.java
@@ -0,0 +1,28 @@
+package homework.lesson3.lilitsharkhatunyan.loop;
+
+/**
+ * Created by lilitsharkhatunyan on 9/23/17.
+ */
+
+public class ArrayUtil {
+    public static void main(String[] args) {
+    }
+
+
+    /**
+     * Shifts each element of the specified array to left such that:
+     * lets initial array is
+     * {1, 2, 3, 4, 5, 6, 7} and shiftSize is 2,
+     * after the method call order of array element should be
+     * {3, 4, 5, 6, 7, 1, 2}
+     * @param a
+     * @param shiftSize
+     */
+    public static void shiftLeft(int a[], int shiftSize) {
+
+    }
+
+
+
+}
+
