44a451b09a53646c3cfe3701c21171d102dcd220
Vardan Khalatyan <vardankhalatyan@workfront.com>
2017-09-24 22:02:30 +0400

    Adding homework java files

--- src/homework/lesson2/vardankhalatyan/util/MathUtil.java
+++ src/homework/lesson2/vardankhalatyan/util/MathUtil.java
@@ -2,64 +2,76 @@ package homework.lesson2.vardankhalatyan.util;
 
 
 public class MathUtil {
-	public static void main(String[] args) {
-	}
+    public static void main(String[] args) {
+    }
 
-	/**
-	 * Returns the value of the first argument raised to the power of the
-	 * second argument, such that a > 0 and n > 0.
-	 *
-	 * @param a the base
-	 * @param n the exponent
-	 * @return the value <code>a<sup>b</sup></code>.
-	 */
-	public static int pow(int a, int n) {
-//		while ( int i<n){
-//			a = a * a;
-//		}
-		return a;
-	}
+    /**
+     * Returns the value of the first argument raised to the power of the
+     * second argument, such that a > 0 and n > 0.
+     *
+     * @param a the base
+     * @param n the exponent
+     * @return the value <code>a<sup>b</sup></code>.
+     */
+    public static int pow(int a, int n) {
+        int i = 1;
+        int x = a;
+            while (i < n) {
+                x = x * a;
+                i++;
+        }
+        return x;
+    }
 
-	/**
-	 * Calculates and returns Greatest common divisor
-	 *
-	 * @param x
-	 * @param y
-	 * @return gcd of specified x and y numbers
-	 */
-	public static int gcd(int x, int y) {
+    /**
+     * Calculates and returns Greatest common divisor
+     *
+     * @param x
+     * @param y
+     * @return gcd of specified x and y numbers
+     */
+    public static int gcd(int x, int y) {
 
-		return 0;
-	}
+        return 0;
+    }
 
-	/**
-	 * Calculates and returns the factorial of specified n number
-	 *
-	 * @param n the number factorial of which must be calculated
-	 * @return calculated number
-	 */
-	public static int factorial(int n) {
-		return 0;
-	}
+    /**
+     * Calculates and returns the factorial of specified n number
+     *
+     * @param n the number factorial of which must be calculated
+     * @return calculated number
+     */
+    public static int factorial(int n) {
+            int x = 1;
+            for (int i = 1; i <= n; i++){
+                x = x * i;
+            }
 
-	/**
-	 * Calculates and returns the absolute value
-	 * of specified n number
-	 *
-	 * @param n specified number to find the absolute number
-	 * @return absolute number of specified n
-	 */
-	public static int abs(int n) {
-		return 0;
-	}
+        return x;
+    }
 
-	public static void printIntAsBinary(int n) {
-		//todo complete
-	}
+    /**
+     * Calculates and returns the absolute value
+     * of specified n number
+     *
+     * @param n specified number to find the absolute number
+     * @return absolute number of specified n
+     */
+    public static int abs(int n) {
+        if (n < 0) {
+            n = n * (-1);
+        }
+        return n;
 
-	public static int reverse(int number) {
+    }
 
-		return 0;
-	}
+    public static void printIntAsBinary(int n) {
+        
+    }
+
+    public static int reverse(int number) {
+            number = number * -1;
+        return number;
+    }
 
 }
---
+++ src/homework/lesson3/vardankhalatyan/additionaltasks/TwoDimensionalArrayUtil.java
@@ -0,0 +1,35 @@
+package homework.lesson3.vardankhalatyan.additionaltasks;
+
+
+
+public class TwoDimensionalArrayUtil {
+
+    public static void main(String[] args) {
+//        int[][] matrix = createSpiralMatrix(21);
+//        print(matrix);
+    }
+
+    /**
+     *  Initializes two dimensional array such as explained below:
+     *  For instance, if odd_size is 7 then the result should be
+     *  the matrix described below:
+     *
+     *   31 30 29 28 27 26 49
+     *   32 13 12 11 10 25 48
+     *   33 14 3  2  9  24 47
+     *   34 15 4  1  8  23 46
+     *   35 16 5  6  7  22 45
+     *   36 17 18 19 20 21 44
+     *   37 38 39 40 41 42 43
+     *
+     * @param size size of columns and rows, should be odd number
+     */
+    public static int[][] createSpiralMatrix (int size) {
+        return null; // TODO implement
+    }
+
+
+    public static void print(int[][] matrix) {
+        //TODO implement
+    }
+}
