4f14c9e163236250f10105faa6cf016483ec7d8d
Vardan Khalatyan <vardankhalatyan@workfront.com>
2017-10-12 10:12:27 +0400

    Updated TwoDimensionalArray file

--- src/homework/lesson3/vardankhalatyan/additionaltasks/TwoDimensionalArrayUtil.java
+++ src/homework/lesson3/vardankhalatyan/additionaltasks/TwoDimensionalArrayUtil.java
@@ -5,65 +5,25 @@ package homework.lesson3.vardankhalatyan.additionaltasks;
 public class TwoDimensionalArrayUtil {
 
     public static void main(String[] args) {
-//        int[][] matrix = createSpiralMatrix(21);
-//        print(matrix);
-//        createSpiralMatrix(5);
-        int[][] matrix = new int[7][7];
-        drawSquare(matrix,3);
-        drawSquare(matrix,1);
-        drawSquare(matrix,5);
-        drawSquare(matrix,7);
-        ptintM(matrix);
+        int[][] matrix = createSpiralMatrix(21);
+        printM(matrix);
+        int[][] nmatrix = createSpiralMatrix(-1);
+        printM(nmatrix);
     }
 
-
-    /**
-     *  Initializes two dimensional array such as explained below:
-     *  For instance, if odd_size is 7 then the result should be
-     *  the matrix described below:
-     *
-     *   31 30 29 28 27 26 49
-     *   32 13 12 11 10 25 48
-     *   33 14 3  2  9  24 47
-     *   34 15 4  1  8  23 46
-     *   35 16 5  6  7  22 45
-     *   36 17 18 19 20 21 44
-     *   37 38 39 40 41 42 43
-     *
-     *   3  2  9
-     *   4  1  8
-     *   5  6  7
-     *
-     *   1 2 3 4 5 6 7 8 9
-     *
-     * @param size size of columns and rows, should be odd number
-     */
     public static int[][] createSpiralMatrix (int size) {
-
-        int nums = size * size;
+        if (size < 1){
+            System.out.println("Please, specify positive array leght > 1");
+            System.out.println("Returned empty two-dimensional array");
+            int[][] a = new int[0][0];
+            return a;
+        }
         int[][] matrix = new int[size][size];
+        int nums = size * size;
         for (int i = 1; i <=size; i++){
             drawSquare(matrix,i);
         }
-        ptintM(matrix);
         return matrix;
-    }
-
-
-
-
-    private static void ptintM(int[][] a){
-        for (int i = 0; i < a.length; i++){
-            for (int j = 0; j < a.length; j++){
-                if (a[i][j] < 10){
-                    System.out.print(a[i][j] + "  ");
-
-                }else{
-                    System.out.print(a[i][j] + " ");
-                }
-            }
-            System.out.println();
-        }
     }
 
     private static void drawSquare(int[][] a ,int radius){
@@ -86,6 +46,23 @@ public class TwoDimensionalArrayUtil {
             a[offset][i] = nums;
             nums--;
         }
+    }
+
+    private static void printM(int[][] a){
+        for (int i = 0; i < a.length; i++){
+            for (int j = 0; j < a.length; j++){
+                if (a[i][j] < 10){
+                    System.out.print(a[i][j] + "   ");
+
+                }else if (a[i][j] < 100){
+                    System.out.print(a[i][j] + "  ");
+                }else if (a[i][j] < 1000){
+                    System.out.print(a[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("\n\n");
     }
 
 }
