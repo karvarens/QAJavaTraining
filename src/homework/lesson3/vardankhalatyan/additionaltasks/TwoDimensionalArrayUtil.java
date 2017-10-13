c002af95879f268d3354e6b1a3501e3125f07920
Karen Vardanyan <karenvardanyan@workfront.com>
2017-10-13 07:02:35 +0400

    Added another solution for the print(int [][] ) method.

--- src/homework/lesson3/vardankhalatyan/additionaltasks/TwoDimensionalArrayUtil.java
+++ src/homework/lesson3/vardankhalatyan/additionaltasks/TwoDimensionalArrayUtil.java
@@ -57,10 +57,10 @@ public class TwoDimensionalArrayUtil {
     private static void printM(int[][] a){
         for (int i = 0; i < a.length; i++){
             for (int j = 0; j < a.length; j++){
-                if (a[i][j] < 10){
+                if (a[i][j] < 10) {
                     System.out.print(a[i][j] + "   ");
 
-                }else if (a[i][j] < 100){
+                } else if (a[i][j] < 100){
                     System.out.print(a[i][j] + "  ");
                 }else if (a[i][j] < 1000){
                     System.out.print(a[i][j] + " ");
@@ -69,6 +69,46 @@ public class TwoDimensionalArrayUtil {
             System.out.println();
         }
         System.out.println("\n\n");
+    }
+
+    private static int getCountOfDigits(int number) {
+        if (number < 10)         return 1;
+        if (number < 100)        return 2;
+        if (number < 1000)       return 3;
+        if (number < 10000)      return 4;
+        if (number < 100000)     return 5;
+        if (number < 1000000)    return 6;
+        if (number < 10000000)   return 7;
+        if (number < 100000000)  return 8;
+        if (number < 1000000000) return 9;
+        return 10;
+    }
+
+    private static final String[] spaces = {
+            " ",
+            "  ",
+            "   ",
+            "    ",
+            "     ",
+            "      ",
+            "       ",
+            "        ",
+            "         ",
+            "          "
+    };
+
+    public static void print(int[][] matrix) {
+        int max = matrix.length * matrix.length;
+        int maxLength = getCountOfDigits(max);
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                int val = matrix[i][j];
+                System.out.print(val + spaces[maxLength - getCountOfDigits(val)] );
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
 }
