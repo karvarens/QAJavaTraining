17ed9e5dd961bcb3560604fa697f34918b103e53
David Gevorgyan <davidgevorgyan@workfront.com>
2017-10-17 01:18:52 +0400

    Lesson5 Improvements.

--- src/homework/lesson5/davidgevorgyan/braceChecker/BraceChecker.java
+++ src/homework/lesson5/davidgevorgyan/braceChecker/BraceChecker.java
@@ -1,51 +1,72 @@
 package homework.lesson5.davidgevorgyan.braceChecker;
 
 public class BraceChecker {
-    String text;
+    private String text;
 
     public BraceChecker(String text){
         this.text = text;
     }
 
+    public static class Braces {
+        int index;
+        char symbol;
+        int rowNumber;
+        int indexInRow;
+
+        Braces(int index, char symbol) {
+            this.index = validateIndex(index);
+            this.symbol = validateSymbol(symbol);
+        }
+
+        public void print(Braces value) {
+            System.out.println(value);
+        }
+
+        private int validateIndex(int index) {
+            if (index < 0) {
+                throw new IllegalArgumentException();
+            } else {
+                return index;
+            }
+        }
+
+        private char validateSymbol(char symbol) {
+            if (symbol != '(' && symbol != '{' && symbol != '[') {
+                throw new IllegalArgumentException();
+            } else {
+                return symbol;
+            }
+        }
+    }
+
     public boolean parse() {
-        Stack bracesStack = new Stack(text.length());
-        Braces temp;
-        char [] bracesOpeningTemplate = {'<', '(' , '[', '{'};
-        char [] bracesClosingTemplate = {'>', ')' , ']', '}'};
+        Stack bracesStack = new Stack();
+        char [] bracesOpeningTemplate = {'(' , '[', '{'};
+        char [] bracesClosingTemplate = {')' , ']', '}'};
 
         for (int i = 0; i < text.length(); i++) {
+            //TODO implement with switch operator
             char tempChar = text.charAt(i);
             //Filling Stack
             for (char templateBrace : bracesOpeningTemplate) {
                 if (tempChar == templateBrace) {
-                    temp = new Braces(i, text.charAt(i));
-                    bracesStack.push(temp);
+                    bracesStack.push(new Braces(i,text.charAt(i)));//TODO ask for better solution
                 }
             }
             //Checking Stack
-            int braceIndexCounter=0; //used for counting polar brace
+            int braceIndexCounter = 0; //used for counting polar brace
             for (char templateBrace : bracesClosingTemplate) {
-                //try-catch to avoid -1 element calling
-                try {
-                    if (tempChar == templateBrace && bracesStack.pop().symbol == bracesOpeningTemplate[braceIndexCounter]) {
-                        bracesStack.removeTopOfStack();
+                 if (tempChar == templateBrace) {
+                    if (bracesStack.pop() == null) {
+                        return false;
                     }
-                }
-                catch (ArrayIndexOutOfBoundsException exception){
-                    return false;
+                    if (bracesStack.pop().symbol == bracesOpeningTemplate[braceIndexCounter]) {
+                            bracesStack.removeTopOfStack();
+                    }
                 }
                 braceIndexCounter++;
             }
         }
-
-
-        if (bracesStack.isEmpty()) {
-            return true;
-        }
-        else {
-            return false;
-        }
-
+        return bracesStack.isEmpty();
     }
-
 }
