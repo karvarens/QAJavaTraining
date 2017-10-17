538d4233257438e22137e9e15e7fcb7a8d7fc79b
Edgar <31990472+EdgarAyvazyan@users.noreply.github.com>
2017-10-17 18:36:44 +0400

    Add files via upload

---
+++ src/homework/lesson5/Stack.java
@@ -0,0 +1,37 @@
+package homeWork.lesson5;
+
+public class Stack {
+    private char[] values;
+    private int tos;
+    private char [] newValues;
+
+    public Stack(int size) {
+        values = new char[size];
+        tos = -1;
+    }
+
+    public void push(char val) {
+        if(tos == values.length - 1){
+            //TODO extend the values
+            newValues = new char [values.length * 3 / 2];
+            System.arraycopy(values, 0, newValues, 0, values.length);
+            values = newValues;
+        }
+
+        values[++tos] = val;
+    }
+
+    public char pop(){
+        if (tos < 0){
+            System.out.println("Stack does not loaded:");
+            return 0;
+        }else{
+            return values[tos--];
+        }
+//        TODO complete
+    }
+
+    public boolean isEmpty(){
+        return tos == -1;
+    }
+}
