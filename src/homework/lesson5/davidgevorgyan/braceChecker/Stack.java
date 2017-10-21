77838670108fb25e577ffff3e9805de6cf3fcae5
Karen Vardanyan <karenvardanyan@workfront.com>
2017-10-21 11:52:57 +0400

    Code refactoring, after the homework review.

--- src/homework/lesson5/davidgevorgyan/braceChecker/Stack.java
+++ src/homework/lesson5/davidgevorgyan/braceChecker/Stack.java
@@ -2,20 +2,20 @@ package homework.lesson5.davidgevorgyan.braceChecker;
 
 public class Stack {
 
-    private BraceChecker.Braces[] values;
+    private BraceChecker.BracketItem[] values;
     private int topOfStack;
 
     Stack() {
-        values = new BraceChecker.Braces[3];
+        values = new BraceChecker.BracketItem[3];   //TODO: see auditorium.braceChecker.Stack
         topOfStack = -1;
     }
 
     Stack(int size) {
-        values = new BraceChecker.Braces[size];
+        values = new BraceChecker.BracketItem[size];
         topOfStack = -1;
     }
 
-    public void push(BraceChecker.Braces value) {
+    public void push(BraceChecker.BracketItem value) {
         if(topOfStack == values.length - 1){
              enlarge();
         }
@@ -27,7 +27,7 @@ public class Stack {
         topOfStack--;
     }
 
-    public BraceChecker.Braces pop(){
+    public BraceChecker.BracketItem pop(){
         if(topOfStack < 0) {
             return null;
         }
@@ -35,7 +35,7 @@ public class Stack {
     }
 
     private void enlarge(){
-        BraceChecker.Braces[] newValues = new BraceChecker.Braces[values.length * 3 / 2];
+        BraceChecker.BracketItem[] newValues = new BraceChecker.BracketItem[values.length * 3 / 2];
         System.arraycopy(values, 0, newValues, 0, values.length);
         values = newValues;
     }
