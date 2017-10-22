2d052308986c97b3179026b0d67866bac66f3c57
ani-ghazaryan <ani.ghazaryan.h@gmil.com>
2017-10-23 00:17:47 +0400

    Added BraceChecker/Stack classes functionality

---
+++ src/homework/lesson5/anighazaryan/braceChecker/Stack.java
@@ -0,0 +1,46 @@
+package homework.lesson5.anighazaryan.braceChecker;
+
+public class Stack {
+
+    private int[] values;
+    private int tos;
+
+    public Stack(int size) {
+        values = new int[size];
+        tos = -1;
+    }
+
+    public void push(int val) {
+        if (tos == values.length - 1) {
+            ensureCapacity();
+        }
+
+        values[++tos] = val;
+    }
+
+    public int pop() {
+        if (tos == -1) {
+            System.out.println("Stack is empty");
+            return 0;
+        }
+        return values[tos--];
+    }
+
+    public int peek() {
+        if (tos == -1) {
+            return 0;
+        }
+        return values[tos];
+    }
+
+    public boolean isEmpty(){
+        return tos == -1;
+    }
+
+    private void ensureCapacity() {
+        int[] newValues = new int[values.length * 3 / 2];
+        System.arraycopy(values, 0, newValues, 0, values.length);
+        values = newValues;
+    }
+
+}
