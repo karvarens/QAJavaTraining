adafcab36ee4496d0ca45a5a4e413efb7715dee1
Karen Vardanyan <karenvardanyan@workfront.com>
2017-11-03 18:43:47 +0400

    Auditorium work and some additional GUI (graphic user interface) classes for the figures application.

---
+++ src/auditorium/figure/Circle.java
@@ -0,0 +1,19 @@
+package auditorium.figure;
+
+import java.awt.*;
+
+abstract public class Circle extends Figure {
+    public Circle(int x, int y, int diameter) {
+        super(x, y, diameter, diameter);
+    }
+
+//    @Override
+    public void draw(Graphics g) {
+
+    }
+
+    @Override
+    public boolean isBelong(int x, int y) {
+        return false;
+    }
+}
---
+++ src/auditorium/figure/FigureFrame.java
@@ -0,0 +1,13 @@
+package auditorium.figure;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FigureFrame extends JFrame {
+
+
+
+
+    public FigureFrame() {
+    }
+}
