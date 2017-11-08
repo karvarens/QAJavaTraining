e53ce5bcb9d6edca3c5d7b1aad0d63e6ef607b61
lilitshark <lilit.sharkhatunyan@yahoo.com>
2017-11-08 13:44:29 +0400

    Minor fixes.

--- src/homework/lesson5/narekvagharshakyan/braceChecker/BraceCheckerTest.java
+++ src/homework/lesson5/narekvagharshakyan/braceChecker/BraceCheckerTest.java
@@ -3,7 +3,7 @@ package homework.lesson5.narekvagharshakyan.braceChecker;
 public class BraceCheckerTest {
     public static void main(String[] args) {
         BraceChecker myBraceChecker = new BraceChecker();
-        String textForChecking = "(sds{ss()()(())s(21245 {}{{}} dsfsd)}dsd)  ([])";
+        String textForChecking = "(sds{ss()()(())s(21245 {}{{}} dsfsd)}dsd) \n ([])";
         myBraceChecker.parse(textForChecking);
         System.out.println();
     }
