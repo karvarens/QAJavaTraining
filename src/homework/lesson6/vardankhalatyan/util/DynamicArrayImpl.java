f739f0e35a5acdd36fbba2f74681067834b4f1b6
Vardan Khalatyan <vardankhalatyan@workfront.com>
2017-12-12 20:08:04 +0400

    not finished yet

--- src/homework/lesson6/vardankhalatyan/util/DynamicArrayImpl.java
+++ src/homework/lesson6/vardankhalatyan/util/DynamicArrayImpl.java
@@ -1,5 +1,7 @@
 package homework.lesson6.vardankhalatyan.util;
 
+import java.util.NoSuchElementException;
+
 public class DynamicArrayImpl<T> implements DynamicArray<T> {
     private static final int DEFAULT_CAPACITY = 16;
 
@@ -9,41 +11,77 @@ public class DynamicArrayImpl<T> implements DynamicArray<T> {
      */
     private int size;
 
-    //TODO: add two constructors: default and with specified capacity.
+    public DynamicArrayImpl() {
+        new DynamicArrayImpl(DEFAULT_CAPACITY);
+    }
+
+    public DynamicArrayImpl(int capacity) {
+        values = new Object[capacity];
+    }
+
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public boolean contains(T o) {
+        for (int i = 0; i < size - 1; i++) {
+            if (o.equals(values[i])) return true;
+        }
         return false;
     }
 
     @Override
     public int indexOf(T o) {
-        return 0;
+        for (int i = 0; i < size - 1; i++) {
+            if (o.equals(values[i])) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public int lastIndexOf(T o) {
-        return 0;
+        for (int i = size - 1; i <= 0; i++) {
+            if (o.equals(values[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private boolean getElement(int index){
+        if (index <= size && index >= 0){
+            return true;
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (getElement(index)) {
+            return (T) values[index];
+        } else {
+            throw new NoSuchElementException("Something went wrong: There is no element in mentioned index");
+        }
     }
 
     @Override
     public T set(int index, T element) {
-        return null;
+        if (getElement(index)){
+            return (T) (values[index] = element);
+        } else {
+            throw new NoSuchElementException("Something went wrong: Can't set the mentioned index element");
+        }
     }
 
     @Override
