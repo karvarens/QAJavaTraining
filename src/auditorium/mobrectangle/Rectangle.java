2c9b85646eb5252e0ead784539b7cdb362ba5dc3
David Gevorgyan <davidgevorgyan@workfront.com>
2017-12-13 15:04:54 +0400

    Mob programming changes

--- src/auditorium/mobrectangle/Rectangle.java
+++ src/auditorium/mobrectangle/Rectangle.java
@@ -5,39 +5,34 @@ public class Rectangle {
     private int width;
     private int height;
 
-    private Rectangle(int width, int height) {
-        if (isValid()) {
+    Rectangle(int width, int height) {
             this.width = width;
             this.height = height;
-        } else {
-            this.width = 1;
-            this.height = 2;
-
-        }
-
     }
 
     private void fullRow() {
+        isValid();
         for (int i = 0; i < width; i++) {
             System.out.print(" * ");
         }
-
         System.out.println();
     }
 
     private void nonFullRow() {
-        for (int i = 0; i < width; i++) {
-            if (i == 0 || i == width - 1) {
-                System.out.print(" * ");
-            } else {
-                System.out.print("   ");
+       isValid();
+            for (int i = 0; i < width; i++) {
+                if (i == 0 || i == width - 1) {
+                    System.out.print(" * ");
+                } else {
+                    System.out.print("   ");
+                }
             }
+        System.out.println();
         }
 
-        System.out.println();
-    }
 
-    void print() {
+    public void print() {
+        isValid();
         for (int i = 0; i < height; i++) {
             if (i == 0 || i == height - 1) {
                 fullRow();
@@ -47,21 +42,28 @@ public class Rectangle {
         }
     }
 
-    private boolean isValid() {
-        if (width > 0 && height > 0) {
-            return true;
-        } else {
-            return false;
+    private void isValid() {
+        if (width <= 0 && height < 0) {
+            throw new IllegalArgumentException("BRRRR- Assigned value is not valid");
         }
     }
 
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(5, 3);
-        //rectangle.fullRow(10);
-        //rectangle.nonFullRow(10);
+        Rectangle rectangle = new Rectangle(7, 3);
         rectangle.print();
-        System.out.println("LLLOL");
+
         Rectangle rectangle2 = new Rectangle(-10, -20);
         rectangle2.print();
+
+        Rectangle rectangle3 = new Rectangle(-10,-11);
+        rectangle3.fullRow();
+    }
+}
+
+class outsideOfRectangle {
+    Rectangle abc = new Rectangle(10,20);
+
+    public void main() {
+        abc.print();
     }
 }
