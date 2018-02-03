ad31e1aa4b8cc54a34a6fe35b01b6a39b145929a
David Gevorgyan <davidgevorgyan@workfront.com>
2018-02-04 01:40:55 +0400

    TreeMaps tasks 8-14

--- src/homework/lesson7/davidgevorgyan/TreeMapDemo.java
+++ src/homework/lesson7/davidgevorgyan/TreeMapDemo.java
@@ -1,9 +1,7 @@
 package homework.lesson7.davidgevorgyan;
 
 
-import java.util.Comparator;
-import java.util.Set;
-import java.util.TreeMap;
+import java.util.*;
 
 public class TreeMapDemo {
     public static void main(String[] args) {
@@ -27,7 +25,15 @@ public class TreeMapDemo {
                 return -1 * o1.compareTo(o2);
             }
         }));
-        
+        System.out.println(task8Greatest(example));
+        System.out.println(task8Least(example));
+        System.out.println(task9Greatest(example));
+        System.out.println(task9Least(example));
+        System.out.println(task10(example));
+        System.out.println(task11(example,"ABC"));
+        System.out.println(task12(example,"ZZZ"));
+        System.out.println(task13(example,"XYW"));
+        System.out.println(task14(example,"ABC", false));
     }
 
     /**
@@ -64,8 +70,6 @@ public class TreeMapDemo {
      * @return search results
      */
 
-
-
     public static <K,V> boolean task3(TreeMap<K,V> original, K key) {
         return original.containsKey(key);
     }
@@ -78,6 +82,7 @@ public class TreeMapDemo {
      * @param <V> Value Type
      * @return search results
      */
+
     public static <K,V> boolean task4(TreeMap<K,V> original, V value) {
         return original.containsValue(value);
     }
@@ -89,6 +94,7 @@ public class TreeMapDemo {
      * @param <V> Value Type
      * @return Set of keys
      */
+
     public static <K,V> Set task5(TreeMap<K,V> original) {
     return original.keySet();
     }
@@ -104,7 +110,6 @@ public class TreeMapDemo {
         original.clear();
     }
 
-
     /**
      * Write a Java program to sort keys in Tree Map by using comparator.
      * @param original original TreeMap
@@ -118,5 +123,116 @@ public class TreeMapDemo {
         TreeMap <K,V> result = new TreeMap<>(comparator);
         result.putAll(original);
         return result;
+    }
+
+    /**
+     * Write a Java program to get a key-value mapping associated with the greatest key in a map.
+     * @param original original TreeMap
+     * @param <K> Key Type
+     * @param <V> Value Type
+     * @return Map entry
+     */
+
+    public static <K,V> Map.Entry task8Greatest(TreeMap<K,V> original) {
+        return original.lastEntry();
+    }
+
+    /**
+     * Write a Java program to get a key-value mapping associated with the least key in a map.
+     * @param original original TreeMap
+     * @param <K> Key Type
+     * @param <V> Value Type
+     * @return Map entry
+     */
+
+    public static <K,V> Map.Entry task8Least(TreeMap<K,V> original) {
+        return original.firstEntry();
+    }
+
+    /**
+     * Write a Java program to get the last (highest) key currently in a map.
+     * @param original original TreeMap
+     * @param <K> Key Type
+     * @param <V> Value Type
+     * @return the last (highest) key
+     */
+
+    public static <K,V> K task9Greatest(TreeMap<K,V> original) {
+        return original.lastKey();
+    }
+
+    /**
+     * * Write a Java program to get the first (lowest) key currently in a map.
+     * @param original original TreeMap
+     * @param <K> Key Type
+     * @param <V> Value Type
+     * @return the first (lowest) key
+     */
+
+    public static <K,V> K task9Least(TreeMap<K,V> original) {
+        return original.firstKey();
+    }
+
+    /**
+     * Write a Java program to get a reverse order view of the keys contained in a given map.
+     * @param original original TreeMap
+     * @param <K> Key Type
+     * @param <V> Value Type
+     * @return reversed keySet
+     */
+
+    public static <K,V> Set <K> task10(TreeMap<K,V> original) {
+        return original.descendingKeySet();
+    }
+
+    /**
+     * Write a Java program to get a key-value mapping associated with the greatest key less than or equal to the given key.
+     * @param original original TreeMap
+     * @param originalKey Key which we are searching for
+     * @param <K> Key Type
+     * @param <V> Value Type
+     * @return Map Entry with search results
+     */
+
+    public static <K,V> Map.Entry task11(TreeMap<K,V> original, K originalKey) {
+        return original.floorEntry(originalKey);
+    }
+
+    /**
+     * Write a Java program to get the greatest key less than or equal to the given key.
+     * @param original original TreeMap
+     * @param originalKey Key which we are searching for
+     * @param <K> Key Type
+     * @param <V> Value Type
+     * @return Key with search results
+     */
+
+    public static <K,V> K task12(TreeMap<K,V> original, K originalKey) {
+        return original.floorKey(originalKey);
+    }
+
+    /**
+     * Write a Java program to get the portion of a map whose keys are strictly less than a given key.
+     * @param original original TreeMap
+     * @param originalKey Key which we are searching for
+     * @param <K> Key Type
+     * @param <V> Value Type
+     * @return map whose keys are strictly less than a given key.
+     */
+    public static <K,V> SortedMap task13(TreeMap<K,V> original, K originalKey) {
+        return original.headMap(originalKey);
+    }
+
+    /**
+     * Write a Java program to get the portion of this map whose keys are less than (or equal to, if inclusive is true) a given key.
+     * @param original original TreeMap
+     * @param originalKey Key which we are searching for
+     * @param <K> Key Type
+     * @param <V> Value Type
+     * @param inclusive display is search is inclusive or not
+     * @return map whose keys are less than (or equal to, if inclusive is true) a given key.
+     */
+    public static <K,V> SortedMap task14(TreeMap<K,V> original, K originalKey, boolean inclusive) {
+        return original.tailMap(originalKey, inclusive);
     }
 }
